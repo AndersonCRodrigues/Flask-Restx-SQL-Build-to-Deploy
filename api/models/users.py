26536015ef0a8d0b0758481d2b97e0a7508e3919
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2024-03-29 15:17:50 -0300

    feat: add get user by id def

--- api/models/users.py
+++ api/models/users.py
@@ -4,7 +4,7 @@ from ..utils import db
 
 class UserModel(db.Model):
 
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(45), nullable=False, unique=True)
     password_hash = db.Column(db.String(45), nullable=False)
@@ -12,7 +12,7 @@ class UserModel(db.Model):
     is_staff = db.Column(db.Boolean(), default=False)
     is_active = db.Column(db.Boolean(), default=False)
     date_created = db.Column(db.DateTime(), default=datetime.now)
-    orders = db.relationship('OrderModel', backref='customer_order', lazy=True)
+    orders = db.relationship("OrderModel", backref="customer_order", lazy=True)
 
     def __repr__(self):
         return f"<User {self.id} {self.username}>"
@@ -20,3 +20,7 @@ class UserModel(db.Model):
     def save(self):
         db.session.add(self)
         db.session.commit()
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.query.get_or_404(id)
