9ee58d25ceb815ddcf083bbf3034108837f88a65
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2024-04-18 14:36:03 -0300

    feat: error handler

--- api/__init__.py
+++ api/__init__.py
@@ -8,6 +8,7 @@ from .models.orders import OrderModel
 from .models.users import UserModel
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+from werkzeug.exceptions import NotFound, MethodNotAllowed
 
 
 def create_app(config=config_dict["dev"]):
@@ -17,13 +18,22 @@ def create_app(config=config_dict["dev"]):
 
     api = Api(app)
 
+    api.add_namespace(order_namespace)
+    api.add_namespace(auth_namespace, path="/auth")
+
     db.init_app(app)
 
     Migrate(app, db)
+
     JWTManager(app)
 
-    api.add_namespace(order_namespace)
-    api.add_namespace(auth_namespace, path="/auth")
+    @api.errorhandler(NotFound)
+    def not_found(error):
+        return {"error": "Not Found"}, 404
+
+    @api.errorhandler(MethodNotAllowed)
+    def method_not_allowed(error):
+        return {"error": "Method Not Allowed"}, 405
 
     @app.shell_context_processor
     def make_shell_context():
