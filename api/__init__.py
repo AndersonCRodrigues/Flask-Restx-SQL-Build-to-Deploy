8fa87ee6b2d7942f7acece6da49911ade40e516f
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2024-05-09 11:59:05 -0300

    feat: API doc authorizations and security

--- api/__init__.py
+++ api/__init__.py
@@ -16,10 +16,21 @@ def create_app(config=config_dict["dev"]):
 
     app.config.from_object(config)
 
+    authorizations = {
+        "Baerer Auth": {
+            "type": "apiKey",
+            "in": "header",
+            "namer": "Authorization",
+            "description": "Add a JWT with ** Bearer &lt;JWT&gt; to authorize",
+        }
+    }
+
     api = Api(
         app,
         title="Pizza Delivery API",
         description="A REST API for a Pizza Delivery service",
+        authorizations=authorizations,
+        security="Bearer Auth",
     )
 
     api.add_namespace(order_namespace)
